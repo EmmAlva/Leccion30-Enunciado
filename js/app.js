f8c6745dd485a14711d7ce3289185ec69c91fc04
EmmAlva <clioemma_28@hotmail.com>
2017-06-05 20:07:47 -0500

    Scroll nav

--- js/app.js
+++ js/app.js
@@ -1,3 +1,24 @@
 /*
  * Archivo principal de JS
  */
+ var lastScrollTop = 0;
+ window.addEventListener("scroll",function(){
+ 	var currentScroll = window.pageYOffset || document.documentElement.scrollTop;
+ 	if(currentScroll > lastScrollTop){
+ 		document.getElementsByTagName("header")[0].classList.remove("nav-top");
+ 		document.getElementsByTagName("header")[0].classList.add("nav-scroll");
+ 	}
+ 	if (currentScroll < 40){
+     document.getElementsByTagName("header")[0].classList.add("nav-top");
+     document.getElementsByTagName("header")[0].classList.remove("nav-scroll");
+   }
+   if (currentScroll > 669 && currentScroll < 1565){
+     document.getElementsByClassName("link-hover")[0].classList.add("active-link");
+     document.getElementsByClassName("link-hover")[1].classList.remove("active-link");
+     document.getElementsByClassName("link-hover")[2].classList.remove("active-link");
+   }
+  
+
+ 	console.log(currentScroll);
+
+ }, false);
