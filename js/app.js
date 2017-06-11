93fd336319d19f3fa1aa1400dae4d6bece0b90a6
EmmAlva <clioemma_28@hotmail.com>
2017-06-11 13:51:34 -0500

    js

--- js/app.js
+++ js/app.js
@@ -1,3 +1,17 @@
 /*
  * Archivo principal de JS
  */
+
+// SCROLL
+var lastScrollTop = 0;
+	window.addEventListener("scroll",function(){
+		var currentScroll = window.pageYOffset|| window.scrollY ||window.body.scrollTop;
+		if(currentScroll>lastScrollTop){
+			classList.add
+			
+		}
+		else{
+			console.log("up");
+		}
+		lastScrollTop = currentScroll;
+	});
